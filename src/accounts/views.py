475c3296415189ad1c716fbe3c460fea991279a1
Jianzhen Long <u5474453@anu.edu.au>
2020-04-12 14:25:43 +1000

    fix the bug for comment submission

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,28 +1,27 @@
+# Create your views here.
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
-
-# Create your views here.
 from .models import Company, Comment
 from .forms import CommentForm
 
 def company_detail(request, slug):
     template_name = 'company_detail.html'
-    post = get_object_or_404(Company, slug=slug)
-    comments = post.comments.filter(active=True)
+    company = get_object_or_404(Company, slug=slug)
+    comments = company.comments.filter(active=True)
     new_comment = None 
     # Comment posted
     if request.method == 'POST':
         comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
+        # if comment_form.is_valid(): this can be used to add CSRF token validation
  
-            # Create Comment object but don't save to database yet
-            new_comment = comment_form.save(commit=False)
+        # Create Comment object but don't save to database yet
+        new_comment = comment_form.save(commit=False)
             # Assign the current post to the comment
-            new_comment.company = company
+        new_comment.company = company
             # Save the comment to the database
-            new_comment.save()
+        new_comment.save()
     else:
-        comment_form = CommentForm() 
+        comment_form = CommentForm()  
  
     return render(request, template_name, {'company': Company,
                                            'comments': comments,
@@ -31,7 +30,7 @@ def company_detail(request, slug):
 
 class companyListView(ListView):
     model = Company
-    template_name = 'company_list.html'
+    template_name = 'company_list.html' 
 
 class companyDetailView(DetailView):
     model = Company
